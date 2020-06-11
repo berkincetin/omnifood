49b922555876eab3da8c7c11a6a0abddb298e7e4
Berkin Cetin <berkincetin@Berkins-MacBook-Pro.local>
2020-06-11 11:56:26 -0400

    Finished website

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -59,23 +59,23 @@ $(document).ready(function() {
 
     /* Animations on Scroll */
     $('.js--wp-1').waypoint(function(direction) {
-        $('.js--wp-1').addClass('animated fadeIn');
+        $('.js--wp-1').addClass('animated fadeIn slow');
     }, {
         offset: '50%'
     })
 
     $('.js--wp-2').waypoint(function(direction) {
-        $('.js--wp-2').addClass('animated fadeInUp');
+        $('.js--wp-2').addClass('animated fadeInUp slower');
     }, {
         offset: '50%'
     })
     $('.js--wp-3').waypoint(function(direction) {
-        $('.js--wp-3').addClass('animated fadeIn');
+        $('.js--wp-3').addClass('animated fadeIn slow');
     }, {
         offset: '50%'
     })
     $('.js--wp-4').waypoint(function(direction) {
-        $('.js--wp-4').addClass('animated pulse');
+        $('.js--wp-4').addClass('animated pulse slow');
     }, {
         offset: '50%'
     })
